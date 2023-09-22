e10351ba87e3bdc55f5135d1931eaa2dcf21b2c2
chiranjeevi-am <chiranjeeviam@gmail.com>
2023-09-23 00:02:50 +0530

    Updated with similar other patterns used to extract info

--- regex3.py
+++ regex3.py
@@ -7,7 +7,16 @@ BMW's gross cost of operating vehicles in FY2021 S1 was $8 billion.
 '''
 
 import re
-pattern = 'FY(\d{4} (?:Q[1-4]|S[1-2]))[^\$]*\$([\d\.]*)'
+pattern = 'FY(\d{4}) (Q[1-4]|S[1-2]) [^\$]* \$([\d\.]*)'
+
+pattern1 = 'FY(\d{4} (?:Q[1-4]|S[1-2])) [^$]* \$([\d\.]*)'
+
+pattern2 = 'FY(\d{4}) (Q[1-4]|S[1-2]) [^$]* \$([\d\.]*)'
 
 t = re.findall(pattern,text)
+t1 =re.findall(pattern1, text)
+
+t2 = re.findall(pattern2,text)
 print(t)
+print(t1)
+print(t2)
