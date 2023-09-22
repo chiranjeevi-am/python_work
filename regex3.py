e5cbab9fadc5cd961482cbf3f0a80770c9ff82bb
chiranjeevi-am <chiranjeeviam@gmail.com>
2023-09-22 23:44:50 +0530

    regex programs

---
+++ regex3.py
@@ -0,0 +1,13 @@
+#3. Companies in europe reports their financial numbers of semi annual basis and you can have a document like this.
+# To exatract quarterly and semin annual period you can use a regex as shown below
+
+text = '''
+Tesla's gross cost of operating lease vehicles in FY2021 Q1 was $4.85 billion.
+BMW's gross cost of operating vehicles in FY2021 S1 was $8 billion.
+'''
+
+import re
+pattern = 'FY(\d{4} (?:Q[1-4]|S[1-2]))[^\$]*\$([\d\.]*)'
+
+t = re.findall(pattern,text)
+print(t)
